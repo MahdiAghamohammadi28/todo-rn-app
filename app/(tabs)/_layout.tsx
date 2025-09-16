76950f5ab9064c71f1799139483d8d2098d41348
Mahdi <mahdiaghamohammadi28@gmail.com>
2025-09-16 19:42:29 +0330

    Add tabBarIcon support for Home and Settings screens using SvgIcons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,23 +1,30 @@
+import SvgIcons from "@/contants/SvgIcons";
 import { Tabs } from "expo-router";
-import React from "react";
 
 export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
         headerShown: false,
+        tabBarHideOnKeyboard: true,
       }}
     >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
+          tabBarIcon: ({ color }) => (
+            <SvgIcons name={"home"} size={24} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
           title: "Settings",
+          tabBarIcon: ({ color }) => (
+            <SvgIcons name={"settings"} size={24} color={color} />
+          ),
         }}
       />
     </Tabs>
