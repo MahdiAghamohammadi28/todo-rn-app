7fd8d47f400d8494dc52ec0743a9accec2e6506e
Mahdi <mahdiaghamohammadi28@gmail.com>
2025-09-16 19:56:52 +0330

    Refactor RootLayout to manage font loading and splash screen visibility

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,36 @@
+import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
 import { StatusBar } from "expo-status-bar";
+import { useEffect } from "react";
 import "react-native-reanimated";
 
 export default function RootLayout() {
+  const [loaded, error] = useFonts({
+    "poppins-regular": require("../assets/fonts/Poppins-Regular.ttf"),
+    "poppins-medium": require("../assets/fonts/Poppins-Medium.ttf"),
+    "poppins-bold": require("../assets/fonts/Poppins-Bold.ttf"),
+    "poppins-black": require("../assets/fonts/Poppins-Black.ttf"),
+    "poppins-light": require("../assets/fonts/Poppins-Light.ttf"),
+  });
+
+  useEffect(() => {
+    if (loaded || error) {
+      SplashScreen.hideAsync();
+    }
+  }, [loaded, error]);
+  const shouldRenderUI = loaded || error;
+
   return (
     <>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      </Stack>
-      <StatusBar style="auto" />
+      {shouldRenderUI ? (
+        <>
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          </Stack>
+          <StatusBar style="auto" />
+        </>
+      ) : null}
     </>
   );
 }
